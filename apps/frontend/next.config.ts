08937053a4b405092579a5467f37930a6859f9ed
ElvisSerafim <elvisdouza@outlook.com>
2026-09-05 01:56:44 +0100

    feat: create frontend structure project

---
+++ apps/frontend/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  outputFileTracingRoot: path.join(
+    path.dirname(fileURLToPath(import.meta.url)),
+    "../..",
+  ),
+};
+
+export default nextConfig;
